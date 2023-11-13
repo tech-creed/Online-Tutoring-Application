ba829fea8a2edccc555f0c1cadf2b8d70765ec54
MUKILAN S <mukilan069@gmail.com>
2023-11-13 18:06:44 +0530

    HTML Structure Change

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,11 +1,10 @@
 <link href="../../../assets/css/style.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-<link href="../../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
 
 <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">Online Tutoring</a></h1>
+      <h1 class="logo me-auto"><a href="/">Online Tutoring</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="../assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
